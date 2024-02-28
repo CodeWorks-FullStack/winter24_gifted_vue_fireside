3a0a0618a138261ac06687e1518c3b099b17816a
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-27 17:48:57 -0700

    raw data dump

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,11 +5,15 @@
         <h1>Gifts</h1>
       </div>
     </section>
+
+    <section class="row">
+      {{ gifts }}
+    </section>
   </div>
 </template>
 
 <script>
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 import Pop from '../utils/Pop.js';
 import { giftsService } from '../services/GiftsService.js'
 import { AppState } from '../AppState.js';
@@ -29,7 +33,7 @@ export default {
       getGifts()
     })
     return {
-
+      gifts: computed(() => AppState.gifts)
     }
   }
 }
